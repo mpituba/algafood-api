bbc16f04ad8a9bde37686492465548579dddcea4
Marcelo Pituba <mpituba@yahoo.com>
2022-12-16 18:18:43 -0300

    4.29. Desafio: modelando e implementando a consulta de recursos 		de restaurantes	 - Retirada de suporte XML - Ajuste no controller para suportar domain.service

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import  com.algaworks.algafood.domain.model.Cozinha;
@@ -36,12 +34,6 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
-	}
-	
-	//Retorna Wrapper apenas para XML, tem modelo próprio
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml() {
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
 	@GetMapping("/{cozinhaId}")
@@ -87,7 +79,7 @@ public class CozinhaController {
 		if (cozinhaAtual != null) {
 			//cozinhaAtual.setNome(cozinha.getNome());
 			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-			cozinhaRepository.salvar(cozinhaAtual);
+			cadastroCozinha.salvar(cozinhaAtual);
 			return ResponseEntity.ok(cozinhaAtual);
 		}
 		
