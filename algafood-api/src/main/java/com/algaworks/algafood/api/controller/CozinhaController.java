f8c0f816195205839422be53d0f5649eb9ab4006
Marcelo Pituba <mpituba@yahoo.com>
2022-12-16 16:40:01 -0300

    4.28. Refatorando a exclusão de cozinhas para usar domain services

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +18,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import  com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
@@ -77,7 +78,7 @@ public class CozinhaController {
 		 * são as propriedades que não devem ser copiadas, pois queremos manter o Id,
 		 * caso contrário teremos um Id nulo e erro ao tentar atualizar.
 		 * Entidade atualizada via BeansUtils e que veio do banco é persistida.
-		 * Retorno é enviado ao Builder dos ReponseEntity
+		 * Retorno é enviado ao Builder do ReponseEntity
 		 * O segundo retorno é para o caso da entidade buscada em banco ser nula.
 		 */
 		
@@ -98,16 +99,14 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> remover (@PathVariable Long cozinhaId) {
 		
 		try {
-			Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
-			if (cozinha !=null) {
-				cozinhaRepository.remover(cozinha);
+			cadastroCozinha.excluir(cozinhaId);
+			return ResponseEntity.noContent().build();
 		
-				return ResponseEntity.noContent().build();
-			}
-			
+		}catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.notFound().build();
-		}catch (DataIntegrityViolationException e) {
+			
+		}catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
-		}
+		}	
 	}
 }
--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -37,8 +38,14 @@ public class CozinhaRepositoryImpl implements CozinhaRepository{
 	
 	@Override
 	@Transactional
-	public void remover(Cozinha cozinha) {
-		cozinha = buscar(cozinha.getId());
+	public void remover(Long id) {
+		Cozinha cozinha = buscar(id);
+		
+		if (cozinha == null) {
+			//O 1 indica a quantidade de registros esperados.
+			throw new EmptyResultDataAccessException(1);
+		}
+		
 		manager.remove(cozinha);
 	}
 	
