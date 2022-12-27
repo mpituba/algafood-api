571987f5c6c28c0ab895900b31a171960b743bee
Marcelo Pituba <mpituba@yahoo.com>
2022-12-27 16:01:22 -0300

    4.34. Finalizando a atualização parcial com a API de Reflections do Spring

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,12 +1,16 @@
 package com.algaworks.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -23,6 +27,7 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -162,5 +167,47 @@ public class RestauranteController {
 		 * corpo da requisição**/
 		return ResponseEntity.notFound().build();
 				
+	}
+	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
+			@RequestBody Map<String, Object> campos) {
+		
+		Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+		
+		if(restauranteAtual == null) {
+			
+			return ResponseEntity.notFound().build();
+			
+		}
+		
+		merge(campos, restauranteAtual);
+		
+		//Chamada ao método PUT
+		return atualizar(restauranteId, restauranteAtual);
+	}
+
+	
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+		
+		//Esta classe mapeia tipos de dados de uma origem para uma entidade destino.
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+		
+		//Copia o Mapa para o objeto Restaurante
+		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			
+			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+			field.setAccessible(true);
+			
+			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+			
+			//System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novoValor);
+			
+			ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			
+		});
+		
 	}
 }
